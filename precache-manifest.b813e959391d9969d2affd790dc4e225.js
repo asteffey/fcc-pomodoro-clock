d3fab38ef99a678154873e0a27cd96dc7625f6b9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-16 04:36:33 +0000

    Deploy asteffey/fcc-pomodoro-clock to github.com/asteffey/fcc-pomodoro-clock.git:gh-pages

---
+++ precache-manifest.b813e959391d9969d2affd790dc4e225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67526453820756144aeb1f617e3fba1",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "c0448e641c4763dbad16",
+    "url": "/fcc-pomodoro-clock/static/css/main.b10769cc.chunk.css"
+  },
+  {
+    "revision": "4347d54743936f1e1ea8",
+    "url": "/fcc-pomodoro-clock/static/js/2.ae6ee0d7.chunk.js"
+  },
+  {
+    "revision": "dfb95eeeafce65b039dd68ba767c21bd",
+    "url": "/fcc-pomodoro-clock/static/js/2.ae6ee0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0448e641c4763dbad16",
+    "url": "/fcc-pomodoro-clock/static/js/main.b2630716.chunk.js"
+  },
+  {
+    "revision": "d504f752ccbadbeb51fb",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.64532136.js"
+  }
+]);
